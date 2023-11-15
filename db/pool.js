f2bed6a57c67420ee77c95c5142a732d06630c42
amirih <hossein.amiri.enterprise@gmail.com>
2023-11-15 16:36:04 -0500

    IP updated

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 export const demoPool = new Pool({
   user: "cs377_students",
-  host: "20.98.38.207",
+  host: "20.80.3.55",
   database: "demo",
   password: "cs377_students_password",
   port: 5432,
@@ -10,7 +10,7 @@ export const demoPool = new Pool({
 
 export const demo3Pool = new Pool({
   user: "cs377_students",
-  host: "20.98.38.207",
+  host: "20.80.3.55",
   database: "demo3",
   password: "cs377_students_password",
   port: 5432,
